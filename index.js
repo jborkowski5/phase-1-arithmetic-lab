9cf6270e412e5905e15091e6d107dd717cd4a0a5
Joseph <borkowski.joseph@gmail.com>
2023-08-01 16:41:39 -0400

    complete

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply); 
 
-// Write your code here
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+let num3 = 18;
+let num4 = 7;
+let mod = num3 % num4;
+console.log(mod);
+
+let set = [9, 13, 20, 18, 8];
+let max = Math.max(...set);
+console.log(max);
